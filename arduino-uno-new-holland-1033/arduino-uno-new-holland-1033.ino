f0919ebaf4305c8f7af5672286826d9f009c4b4f
Gary Gurney <ggurney13@churchofjesuschrist.org>
2021-08-24 06:41:44 -0600

    final changes before field tests

--- arduino-uno-new-holland-1033/arduino-uno-new-holland-1033.ino
+++ arduino-uno-new-holland-1033/arduino-uno-new-holland-1033.ino
@@ -7,12 +7,10 @@
 #define A1_RPWM 3            // Arduino pin 3 to power controller RPWM pin 1
 #define A1_LPWM 5            // Arduino pin 5 to power controller LPWM pin 2
 #define A1_POT_IN A0         // arduino pin A0 to actuator feedack potentiometer
-// #define A1_MAX_LIMIT 590     // maximum distance actuator can travel without binding (must be less than or equal to A1_MAX_POT_VAL) 
-// #define A1_MIN_LIMIT 30      // minimum distance actuator can travel without binding (must be greater than or equal to A1_MIN_POT_VAL)
 #define A1_MAX_LIMIT 900     // maximum distance actuator can travel without binding (must be less than or equal to A1_MAX_POT_VAL) 
 #define A1_MIN_LIMIT 30      // minimum distance actuator can travel without binding (must be greater than or equal to A1_MIN_POT_VAL)
-#define A1_MAX_POT_VAL 900   // maximum pot value actuator can provide
-#define A1_MIN_POT_VAL 30    // minimum pot value actuator can provide
+#define A1_MAX_POT_VAL 900   // maximum position value actuator can provide
+#define A1_MIN_POT_VAL 30    // minimum position value actuator can provide
 #define A1_SLOP 5            // +/- range for close enough
 #define LATCH_POINT 250      // latch point for retracting
 
@@ -53,10 +51,12 @@ void updateActuatorPosition(int actuatorPotIn,
   //Serial.print("controllerPotValue: ");
   //Serial.println(controllerPotValue);
 
-  // map controller range to actuator range, example: 1023 on controller maps to 900 actuator, that way we can use the full motion of the controller
-  // controllerPotValue = map(controllerPotValue,actuatorMinPotValue,actuatorMaxPotValue,actuatorCalibratedMin,actuatorCalibratedMax);
-  
-  // motion range range is: 640, min: 30, max: 900 so: 870/2 is center, then add -/+ 320 to both sides, 435+320=755 max and 435-320=115 min
+  // map controller range to actuator range, example: 1023 on controller maps to 755 on actuator, that way we can use the full motion of the controller
+  // note that we can't use the full range of motion on the actuator, the motion range on the 1033 pick-up lever matches the actuator range from 115 to 755
+  //
+  // total motion range range for 1033 pick-up is: 640. The min value provided by actuator is: 30 and the value provided by atuator is: 900 
+  // therefore: 870/2 is center (which is at 435) on the actuator, then add -/+ 320 to both sides of center then we have 435+320=755 max for 
+  // actuator and 435-320=115 min for actuator. Map the controller's full range (0,1023) over the range of the actuator (115 to 755)
   controllerPotValue = map(controllerPotValue,0,1023,115,755);
 
   // close enough, don't do anything
@@ -67,7 +67,7 @@ void updateActuatorPosition(int actuatorPotIn,
   }
 
   // retract (extended too far)
-  // todo: exponential response position response?
+  // todo: exponential position response?
   /*
    * For input and output ranges that go from -1 to +1, the equation is: output = ( (1 - factor) x input3 ) + ( factor x input )
    * Source https://www.physicsforums.com/threads/equation-required-to-calculate-exponential-rate.524002/
@@ -82,6 +82,7 @@ void updateActuatorPosition(int actuatorPotIn,
     }    
 
     // make speed a function of how close we are to the target value
+    // if we don't do this then the actuator will overshoot the target
     long speed = MOTOR_SPEED;
     long diff = actuatorPotValue - controllerPotValue;
     if (diff < 75) {
@@ -128,6 +129,7 @@ void updateActuatorPosition(int actuatorPotIn,
       }
   
       // make speed a function of how close we are to the target value
+      // if we don't do this then the actuator will overshoot the target
       long speed = MOTOR_SPEED;
       long diff = controllerPotValue - actuatorPotValue;;
       if (diff < 75) {
